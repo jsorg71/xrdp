9283dc0bb012ea9ad9dea0df7b0a4a8bc1eb95d0
Jay Sorg <jay.sorg@gmail.com>
2023-10-22 00:08:30 -0700

    work on waitforx

--- waitforx/waitforx.c
+++ waitforx/waitforx.c
@@ -1,3 +1,8 @@
+
+#if defined(HAVE_CONFIG_H)
+#include "config_ac.h"
+#endif
+
 #include <X11/extensions/Xrandr.h>
 #include <signal.h>
 #include <stdio.h>
@@ -5,13 +10,17 @@
 #include <sys/signal.h>
 #include <unistd.h>
 
-#define ATTEMPTS 10
+#include "arch.h"
+#include "os_calls.h"
+
+#define DISPLAY_ATTEMPTS 10
+#define RANDR_ATTEMPTS 2
 #define ALARM_WAIT 30
 
 void
 alarm_handler(int signal_num)
 {
-    printf("Unable to find RandR outputs after %d seconds\n", ALARM_WAIT);
+    g_writeln("waitforx: Unable to find RandR outputs after %d seconds", ALARM_WAIT);
     exit(1);
 }
 
@@ -23,7 +32,7 @@ main(int argc, char **argv)
     int event_base = 0;
     int n = 0;
     int outputs = 0;
-    int wait = ATTEMPTS;
+    XEvent ev;
 
     Display *dpy = NULL;
     XRRScreenResources *res = NULL;
@@ -35,17 +44,17 @@ main(int argc, char **argv)
 
     if (!display)
     {
-        printf("DISPLAY is null");
-        exit(1);
+        g_writeln("waitforx: DISPLAY is null");
+        return 1;
     }
 
-    for (n = 1; n <= wait; ++n)
+    for (n = 1; n <= DISPLAY_ATTEMPTS; ++n)
     {
         dpy = XOpenDisplay(display);
-        printf("Opening display %s. Attempt %d of %d\n", display, n, wait);
+        g_writeln("waitforx: Opening display %s. Attempt %d of %d", display, n, DISPLAY_ATTEMPTS);
         if (dpy != NULL)
         {
-            printf("Opened display %s\n", display);
+            g_writeln("waitforx: Opened display %s", display);
             break;
         }
         sleep(1);
@@ -53,44 +62,62 @@ main(int argc, char **argv)
 
     if (!dpy)
     {
-        printf("Unable to open display %s\n", display);
-        exit(1);
+        g_writeln("waitforx: Unable to open display %s", display);
+        return 1;
     }
 
-    if (!XRRQueryExtension(dpy, &event_base, &error_base))
+    for (n = 1; n <= RANDR_ATTEMPTS; ++n)
     {
-        printf("RandR not supported on display %s", display);
+        g_writeln("waitforx: Waiting for RandR extension.  Attempt %d of %d", n, RANDR_ATTEMPTS);
+        if (XRRQueryExtension(dpy, &event_base, &error_base))
+        {
+            g_writeln("waitforx: RandR supported on display %s", display);
+            break;
+        }
+        event_base = 0;
+        sleep(1);
+    }
+    if (event_base == 0)
+    {
+        g_writeln("waitforx: RandR not supported on display %s", display);
+        return 0; /* ok */
+    }
+
+    XRRSelectInput(dpy, DefaultRootWindow(dpy), RROutputChangeNotifyMask);
+
+    for (n = 0; 1; ++n)
+    {
+        res = XRRGetScreenResources(dpy, DefaultRootWindow(dpy));
+        g_writeln("waitforx: Waiting for outputs. Attempt %d", n);
+        if (res != NULL)
+        {
+            if (res->noutput > 0)
+            {
+                outputs = res->noutput;
+                XRRFreeScreenResources(res);
+                g_writeln("waitforx: Found %d output[s]", outputs);
+                break;
+            }
+            XRRFreeScreenResources(res);
+        }
+        while (XNextEvent(dpy, &ev))
+        {
+            if (ev.type == event_base + RRNotify)
+            {
+                break;
+            }
+        }
+    }
+
+    if (outputs > 0)
+    {
+        g_writeln("waitforx: display %s ready with %d outputs", display, res->noutput);
     }
     else
     {
-        for (n = 1; n <= wait; ++n)
-        {
-            res = XRRGetScreenResources(dpy, DefaultRootWindow(dpy));
-            printf("Waiting for outputs. Attempt %d of %d\n", n, wait);
-            if (res != NULL)
-            {
-                if (res->noutput > 0)
-                {
-                    outputs = res->noutput;
-                    XRRFreeScreenResources(res);
-                    printf("Found %d output[s]\n", outputs);
-                    break;
-                }
-                XRRFreeScreenResources(res);
-            }
-            sleep(1);
-        }
-
-        if (outputs > 0)
-        {
-            printf("display %s ready with %d outputs\n", display, res->noutput);
-        }
-        else
-        {
-            printf("Unable to find any outputs\n");
-            exit(1);
-        }
+        g_writeln("waitforx: Unable to find any outputs");
+        return 1;
     }
 
-    exit(0);
+    return 0;
 }
